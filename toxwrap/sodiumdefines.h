f9c06f7e3381c501f14cd48f0723d2bf66421ba1
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-21 20:46:19 -0300

    Corrections to sodiumdefines: now it finally builds.

--- toxwrap/sodiumdefines.h
+++ toxwrap/sodiumdefines.h
@@ -1,5 +1,2 @@
-#define crypto_scalarmult_base crypto_scalarmult_curve25519_base 
-#define crypto_scalarmult crypto_scalarmult_curve25519
-#define crypto_stream_xor crypto_stream_salsa20_xor
-#define crypto_stream crypto_stream_salsa20
-
+#define crypto_scalarmult_curve25519 crypto_scalarmult_curve25519_ref
+#define crypto_scalarmult_curve25519_base crypto_scalarmult_curve25519_ref_base

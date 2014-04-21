b0287a41fee42c626ec6f9f6e0df6b425a1534c0
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-21 18:31:20 -0300

    Some more defines to get around the confusing libsodium defines.

---
+++ toxwrap/sodiumdefines.h
@@ -0,0 +1,5 @@
+#define crypto_scalarmult_base crypto_scalarmult_curve25519_base 
+#define crypto_scalarmult crypto_scalarmult_curve25519
+#define crypto_stream_xor crypto_stream_salsa20_xor
+#define crypto_stream crypto_stream_salsa20
+

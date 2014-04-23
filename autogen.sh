c963872a7b1d09d3f5624d0c998af7024ded081a
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-22 21:20:05 -0300

    Toxcore patch to make it websocket-compatible. Shouldn't break things, I hope.

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,6 @@ echo "Checking for emscripten:"
 which emcc || (echo "emscripten not found" && exit)
 git submodule update --init || exit
 (cd libsodium; ./autogen.sh; emconfigure ./configure) || exit
+echo "Patching toxcore. Answer 'n' if reverse patch detected"
+patch -d toxcore -p1 < toxcore.patch
 make -C toxwrap

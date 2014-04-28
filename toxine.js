fc35b5919f8e2e333df0bb1e8d3256d3a556277d
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-28 17:48:55 -0400

    Hide UI till loaded.

--- toxine.js
+++ toxine.js
@@ -26,9 +26,6 @@ const UPDATE_INTERVAL = 250; //milliseconds
 var tox = null;
 var flashTimeOut = null;
 
-// JS detected!
-$('#wrap').css('visibility', 'visible');
-
 // Setup main entry points
 $(window).on('load', setup);
 $(window).on('beforeunload', cleanup);
@@ -65,6 +62,7 @@ function setup()
 {  
     setupUI();
     $('#flash').fadeOut();
+    $('#wrap').css('display', 'initial');
     setupTox();
 }
 
@@ -286,7 +284,7 @@ function saveUI()
 function save()
 {
     console.log('Saving credentials');
-    if (!sessionStorage.password)
+    if (sessionStorage.password == null || !sessionStorage.password)
     {
         sessionStorage.password = '';
         localStorage.encrypted = false;

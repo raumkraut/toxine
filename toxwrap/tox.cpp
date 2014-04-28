db4f9e8bf3fd2b2c25f417a1d57a6fbc65eb943b
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-28 15:22:08 -0400

    Check for const-correctness in tox.cpp. Also, since the API is a bit more stable, added a few more comments.

--- toxwrap/tox.cpp
+++ toxwrap/tox.cpp
@@ -27,7 +27,6 @@
 #include <vector>
 #include <cstdlib>
 #include <cstdio>
-#include <cstdbool>
 
 extern "C"
 {
@@ -40,31 +39,35 @@ extern "C"
 #define DEBUG_PRINT(...) {}
 #endif
 
+/// Time to wait for something to happen at the tox socket.
 #define WAITPERIOD 100 //ms
-#define STRING_MAX_LENGTH 2000
 
 using std::string;
 using std::vector;
 
+/// Contains contact ("friend") information
 struct Contact
 {
-    int     _fn;
-    string id;
-    string name;
-    string status_message;
-    int     status;
-    time_t  last_online;
+    int     _fn;            ///< Used internally
+    string id;              ///< Tox ID as hexadecimal string
+    string name;            ///< Self-set display name
+    string status_message;  ///< Status message text
+    int     status;         ///< User status: 0: none, 1: away, 2: busy
+    time_t  last_online;    ///< Timestamp of user last seen online. 0 if never seen.
 };
 
+/* Stuff used internally */
 Tox  * tox = NULL;
 uint8_t * tox_data = NULL;
+
+/* temporary buffers to generate strings*/
 uint8_t * tmp_data = NULL;
-
-vector<Contact> contacts;
-
 uint8_t tmp_id[TOX_CLIENT_ID_SIZE];
 uint8_t tmp_name[TOX_MAX_NAME_LENGTH];
 uint8_t tmp_status_message[TOX_MAX_STATUSMESSAGE_LENGTH];
+
+/* list of Contacts, maybe a fn->Contact map would be better if these tend to be long */
+vector<Contact> contacts;
 
 /***** STUBS */
 
@@ -72,11 +75,13 @@ extern "C"
 {
     void srandom(unsigned int seed)
     {
+        /* Random is seeded in toxine.js (and by the browser, too, I hope) */
         DEBUG_PRINT("srandom stub called\n");
     }
 
     int getsockopt(int sockfd, int level, int optname, void * optval, socklen_t * optlen)
     {
+        /* TODO write optval or figure out what this does in general */
         DEBUG_PRINT("getsockopt stub called\n");
         return 0;
     }
@@ -84,6 +89,8 @@ extern "C"
 
 /***** HELPERS */
 
+// Return index in contacts that has request _fn
+// Again, maybe a map would be a better idea
 int find_in_contacts(int fn)
 {
     for (int i = 0; i < contacts.size(); i++)
@@ -93,11 +100,7 @@ int find_in_contacts(int fn)
     return 0;
 }
 
-void contact_changed(int i)
-{
-    EM_ASM(tox.onProfileChanged($0), i);
-}
-
+/// Return data hexadecimally encoded into hexstr
 uint8_t * hexstr_to_data(string hexstr)
 {
     int outlen = hexstr.length() / 2;
@@ -117,11 +120,13 @@ uint8_t * hexstr_to_data(string hexstr)
     return tmp_data;
 }
 
+/// Return id hexadecimally encoded into hexstr
 uint8_t * hexstr_to_id(string hexstr)
 {
     return hexstr_to_data(hexstr);
 }
 
+/// Return hexadecimal representation of the data passed in the parameter
 string data_to_hexstr(const uint8_t * data, int inlen)
 {
     char hexstr[2 * inlen];
@@ -134,6 +139,7 @@ string data_to_hexstr(const uint8_t * data, int inlen)
     return string(hexstr, 2 * inlen);
 }
 
+/// Return hexadecimal representation of the tox id (full address including nospam and checksum)
 string id_to_hexstr(const uint8_t * id)
 {
     return data_to_hexstr(id, TOX_FRIEND_ADDRESS_SIZE);
@@ -141,21 +147,35 @@ string id_to_hexstr(const uint8_t * id)
 
 /* Actual callback functions, invoke JS code */
 
+/// Signal contact changed
+//  Javascript should implement tox.onProfileChanged(i)
+//  i is an index into the Contacts list (not fn!)
+void contact_changed(int i)
+{
+    EM_ASM(tox.onProfileChanged($0), i);
+}
+
 extern "C"
 {
+/// Signal a friend request received
+//  TODO!
     void friend_request(Tox * tox, uint8_t * public_key, uint8_t * data, uint16_t length, void *)
     {
         EM_ASM(tox.onRequest('$0', '$1'), key, data);
         //int32_t tox_add_friend_norequest(tox, uint8_t *client_id);
     }
 
+/// Signal a message received
+//  Javascript should implement tox.onMessage(i, msg)
+//  i is an index into the Contacts list, msg a string
     void friend_message(Tox * tox, int fn, uint8_t * msg, uint16_t length, void *)
     {
         int i = find_in_contacts(fn);
         DEBUG_PRINT("Message from friend %d: %s\n", i, string((char *)msg, length).c_str());
         EM_ASM(tox.onMessage($0, '$1'), i, string((char *)msg, length));
     }
-
+    
+// A contact changed name
     void name_change(Tox * tox, int fn, uint8_t * name, uint16_t length, void *)
     {
         int i = find_in_contacts(fn);
@@ -163,6 +183,7 @@ extern "C"
         contact_changed(i);
     }
 
+// A contact changed their status message
     void status_message(Tox * tox, int fn, uint8_t * msg, uint16_t length, void * d)
     {
         int i = find_in_contacts(fn);
@@ -172,7 +193,8 @@ extern "C"
 }
 
 /***** EXPORTED FUNCTIONS */
-
+/// Update tox.
+//  Call at least a couple times per second, but no more often than every WAITPERIOD ms.
 bool update()
 {  
     bool ret = true;
@@ -192,6 +214,7 @@ bool update()
     return ret;
 }
 
+/// Kill tox and delete buffers from heap.
 void cleanup()
 {
     tox_kill(tox);
@@ -201,11 +224,14 @@ void cleanup()
         delete[] tmp_data;
 }
 
+/// Setup tox and install callbacks.
 bool setup()
 {
     if (tox)
         cleanup();
-        
+    
+    // IPv6 doesn't seem to work. I get strange ws:// addresses
+    // TODO check this
     tox = tox_new(0);
     if (!tox)
     {
@@ -224,54 +250,64 @@ bool setup()
     return true;
 }
 
-bool bootstrap(string address, int port, string id)
+/// Bootstrap from specified DHT node
+bool bootstrap(const string & address, int port, const string & id)
 {
-    return tox_bootstrap_from_address(tox, address.c_str(), TOX_ENABLE_IPV6_DEFAULT, htons(port), hexstr_to_id(id));
+    // IPv6 disabled, see comments in setup() above
+    return tox_bootstrap_from_address(tox, address.c_str(), 0, htons(port), hexstr_to_id(id));
 }
 
-bool addContact(string id, string msg)
+/// Send a contact request, with optional message
+bool addContact(const string & id, const string & msg)
 {
     /* tox-core isn't const-correct, ugly cast is necessary */
     uint8_t * cstr = (uint8_t *)msg.c_str();
     return tox_add_friend(tox, hexstr_to_id(id), cstr, msg.length());
 }
 
+/// Delete contact from list
 bool removeContact(int i)
 {
     return tox_del_friend(tox, contacts[i]._fn);
 }
 
-bool sendMessage(int i, string msg)
+/// Send a message to contact
+bool sendMessage(int i, const string & msg)
 {
     /* tox-core isn't const-correct, ugly cast is necessary */
     uint8_t * cstr = (uint8_t *)msg.c_str();
     return tox_send_message(tox, contacts[i]._fn, cstr, msg.length());
 }
 
+/// Get the user's full Tox ID
 string getId()
 {
     tox_get_address(tox, tmp_id);
     return id_to_hexstr(tmp_id);
 }
 
-bool setName(string name)
+/// Set the user's name
+bool setName(const string & name)
 {
     /* tox-core isn't const-correct, ugly cast is necessary */
     return tox_set_name(tox, (uint8_t *)name.c_str(), name.length()) >= 0;
 }
 
+/// Returns the user's name
 string getName()
 {
     uint16_t n = tox_get_self_name(tox, tmp_name);
     return string((char *)tmp_name, n);
 }
 
-bool setStatusMessage(string msg)
+/// Set the user's status message
+bool setStatusMessage(const string & msg)
 {
     /* tox-core isn't const-correct, ugly cast is necessary */
     return tox_set_status_message(tox, (uint8_t *)msg.c_str(), msg.length()) >= 0;
 }
 
+/// Returns the user's status message
 string getStatusMessage()
 {
     int n = tox_get_self_status_message(tox, tmp_status_message, TOX_MAX_STATUSMESSAGE_LENGTH);
@@ -283,27 +319,33 @@ string getStatusMessage()
     return string((char *)tmp_status_message, n);
 }
 
+/// Sets or unsets the user's typing flag
+//  This is fully client-handled
 bool setTyping(int contact, bool typing)
 {
     return tox_set_user_is_typing(tox, find_in_contacts(contact), typing) >= 0;
 }
 
-/* none, away, busy */
+/// Set the user's status (one of 0: none, 1: away, 2: busy)
 bool setStatus(int status)
 {
     return tox_set_user_status(tox, status) >= 0;
 }
 
+/// Returns the user's status (one of 0: none, 1: away, 2: busy)
 int getStatus()
 {
     return tox_get_self_user_status(tox);
 }
 
+/// Changes the user's Tox ID NOSPAM value
 void changeNospam()
 {
     tox_set_nospam(tox, rand());
 }
 
+/// Returns a list of Contacts
+// Contact-related functions take an index into this list as parameter
 const vector<Contact> & getContacts()
 {
     int n = tox_count_friendlist(tox);
@@ -342,7 +384,10 @@ const vector<Contact> & getContacts()
     return contacts;
 }
 
-string save(string key)
+/// Save tox data to a hexencoded string and return that
+// If key is != "", encrypt it using that key.
+// TODO: Binary option
+string save(const string & key)
 {
     int size;
     if (!key.empty())
@@ -364,7 +409,10 @@ string save(string key)
     return ret;
 }
 
-bool load(const string datastr, string key)
+/// Load tox data from datastr (hex-encoded)
+// If key is != "", decrypt it using that key.
+// TODO: Binary option
+bool load(const string & datastr, const string & key)
 {
     int ret;
     /* tox-core isn't const-correct, ugly cast is necessary */
@@ -377,6 +425,9 @@ bool load(const string datastr, string key)
     return ret == 0;
 }
 
+
+// Create javascript bindings.
+// We use the same camelCase function names as above.
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(tox)

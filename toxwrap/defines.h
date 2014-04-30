7aeca146ac32370943f1ad2742154612b452d68a
Camilo Polymeris <cpolymeris@gmail.com>
2014-04-30 01:32:50 -0400

    Build opus and toxav

--- toxwrap/defines.h
+++ toxwrap/defines.h
@@ -1,2 +1,6 @@
+/* opus stuff. TODO: figure out what this stuff means and if other defines would be better */
+#define OPUS_BUILD
+#define VAR_ARRAYS
+/* libsodium stuff: */
 #define crypto_scalarmult_curve25519 crypto_scalarmult_curve25519_ref
 #define crypto_scalarmult_curve25519_base crypto_scalarmult_curve25519_ref_base
